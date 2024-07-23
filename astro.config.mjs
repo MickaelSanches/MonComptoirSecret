61da6f0311c159ab529da3e4f2cbf94a9605a35a
MikEight <mickael.4870@gmail.com>
2024-07-23 15:40:45 +0200

    chore: revert to initial Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({
-    adapter: vercel(),
-});
+export default defineConfig({});
