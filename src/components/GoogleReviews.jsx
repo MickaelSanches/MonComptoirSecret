9f0ab67fa9bc07e2834b2b91db213df25f78ebda
MikEight <mickael.4870@gmail.com>
2024-07-24 16:21:38 +0200

    fix: Make GoogleReviews component more responsive and reduce its size
    - Added responsive styles for GoogleReviews component
    - Reduced the maximum width of the GoogleReviews component
    - Integrated updated GoogleReviews component into the Astro page

--- src/components/GoogleReviews.jsx
+++ src/components/GoogleReviews.jsx
@@ -1,23 +1,35 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 const GoogleReviews = () => {
   useEffect(() => {
-    // Ajoutez ce script uniquement si Elfsight n'a pas encore été chargé
-    if (!document.querySelector('script[src="https://static.elfsight.com/platform/platform.js"]')) {
-      const script = document.createElement('script');
-      script.src = "https://static.elfsight.com/platform/platform.js";
-      script.dataset.useServiceCore = true;
-      script.defer = true;
-      document.body.appendChild(script);
-    }
+    const script = document.createElement('script');
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.defer = true;
+    script.setAttribute('data-use-service-core', '');
+    document.body.appendChild(script);
+    
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md mx-auto" style={{ width: '50%', height: '30%'}}>
-      <h2 className="text-2xl font-bold mb-4">Avis Google</h2>
-      <div className="elfsight-app-26e0cd9c-145e-4ff6-8e65-ce5b61c50216" data-elfsight-app-lazy ></div>
+    <div className="google-reviews-container">
+      <style jsx>{`
+        .google-reviews-container {
+          width: 100%;
+          max-width: 700px;
+          margin: auto;
+          padding: 20px;
+        }
+        .elfsight-app-26e0cd9c-145e-4ff6-8e65-ce5b61c50216 {
+          max-width: 100%;
+        }
+      `}</style>
+      <div className="elfsight-app-26e0cd9c-145e-4ff6-8e65-ce5b61c50216" data-elfsight-app-lazy></div>
     </div>
   );
 };
 
 export default GoogleReviews;
+
