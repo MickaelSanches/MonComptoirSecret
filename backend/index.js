1397de9c2d1890e92e1b80bfef7db751ce594e26
MikEight <mickael.4870@gmail.com>
2024-08-01 13:28:07 +0200

    Delete Newsletter for Suivez-Nous

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(helmet.frameguard({ action: 'sameorigin' }));
 
 app.use(cors({
-  origin: 'http://localhost:4321',
+  origin: ['http://localhost:4321','https://mon-comptoir-secret.vercel.app/'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
@@ -31,34 +31,6 @@ app.post('/newsletter', async (req, res) => {
     console.error('Erreur lors de l\'inscription à la newsletter:', error);
     res.status(500).json({ error: 'Erreur interne du serveur' });
   }
-});
-
-// Endpoint de contact utilisant EmailJS
-app.post('/contact', async (req, res) => {
-  const { name, email, message } = req.body;
-
-  if (!name || !email || !message) {
-    return res.status(400).json({ error: 'Tous les champs sont obligatoires' });
-  }
-
-  const serviceID = process.env.EMAILJS_SERVICE_ID;
-  const templateID = process.env.EMAILJS_TEMPLATE_ID;
-  const userID = process.env.EMAILJS_USER_ID;
-
-  const templateParams = {
-    from_name: name,
-    from_email: email,
-    message: message,
-  };
-
-  emailjs.send(serviceID, templateID, templateParams, userID)
-    .then(() => {
-      res.status(200).json({ message: 'Votre message a été envoyé avec succès.' });
-    })
-    .catch((error) => {
-      console.error('Erreur lors de l\'envoi de l\'email:', error);
-      res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email' });
-    });
 });
 
 app.listen(port, () => {
