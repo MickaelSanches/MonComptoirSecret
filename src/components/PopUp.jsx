9d6f36d8ddf4524c3759f71483dba3ff83a7af37
MikEight <mickael.4870@gmail.com>
2024-08-01 18:53:28 +0200

    - Popup modification

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -9,11 +9,11 @@ const PromoPopup = () => {
 
     return (
     isVisible && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 transition-opacity duration-500 ease-in-out">
-        <div className="relative bg-white p-4 rounded-lg shadow-lg max-w-sm text-center animate-popup">
+        <div className="fixed inset-0 flex items-center justify-center z-[9999] bg-black bg-opacity-50 transition-opacity duration-500 ease-in-out">
+        <div className="relative bg-white p-4 shadow-lg max-w-sm text-center animate-popup">
             <button
             onClick={closePopup}
-            className="absolute top-[-20px] right-[-20px] text-black bg-white p-2 rounded-full shadow-lg hover:bg-gray-200 transition-all"
+            className="absolute top-[-20px] right-[-20px] text-black bg-white p-2 shadow-lg hover:bg-gray-300 transition-all"
             >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ const PromoPopup = () => {
                 viewBox="0 0 24 24"
                 strokeWidth={2}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className="w-4 h-4"
             >
                 <path
                 strokeLinecap="round"
@@ -30,14 +30,16 @@ const PromoPopup = () => {
                 />
             </svg>
             </button>
-            <img src="/img/1.webp" alt="Promotion" className="mb-4 mx-auto" />
-            <h2 className="text-xl font-bold mb-2 text-black">
-            Dernières Promotions
-            </h2>
-            <p className="text-black">
-            Profitez de nos dernières offres spéciales sur les soins et produits
-            !
-            </p>
+            <video className="mb-4 mx-auto" autoPlay loop muted>
+                        <source src="/img/nouveaute.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    {/* <h2 className="text-xl font-bold mb-2 text-black">
+                        Dernières Promotions
+                    </h2>
+                    <p className="text-black">
+                        Profitez de nos dernières offres spéciales sur les soins et produits
+                    </p> */}
         </div>
         </div>
     )
