7c51ea2148a2e4c2c8126bcf4163349627bb2666
MikEight <mickael.4870@gmail.com>
2024-09-18 21:16:38 +0200

    Updated information in the newsticker

--- src/components/NewsTicker.jsx
+++ src/components/NewsTicker.jsx
@@ -2,15 +2,16 @@ const NewsTicker = () => {
     return (
         <div className="tracking-wider bg-gradientStart text-gray-800 p-2">
             <marquee behavior="scroll" scrollamount="10">
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; 
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; 
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; 
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; 
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; 
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le Photorajeunissement sont arrivés dans votre institut ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant 📅
+                🎉 ANNIVERSAIRE 🎉 Votre institut fête son anniversaire avec des offres spéciales ! 🎂 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le photorajeunissement sont maintenant disponibles ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant ! 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                🚨 COMING SOON 🚨 La Cryolipolyse arrive bientôt dans votre institut ❄️ Préparez-vous à affiner votre silhouette avec cette technologie révolutionnaire ! &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ⚡ COMING SOON ⚡ L'Électrostimulation bientôt disponible 💪 Tonifiez votre corps avec cette nouvelle prestation ultra-efficace ! &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ☀️ L'heure du Summer Body 2025 à sonné ! ☀️ C'est maintenant que tout commence ! Restez connectés 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                🎉 ANNIVERSAIRE 🎉 Votre institut fête son anniversaire avec des offres spéciales ! 🎂 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ✨ NOUVEAUTÉ ✨ L'épilation définitive et le photorajeunissement sont maintenant disponibles ! 💆‍♀️ Réservez votre bilan OFFERT dès maintenant ! 📅 &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                🚨 COMING SOON 🚨 La Cryolipolyse arrive bientôt dans votre institut ❄️ Préparez-vous à affiner votre silhouette avec cette technologie révolutionnaire ! &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ⚡ COMING SOON ⚡ L'Électrostimulation bientôt disponible 💪 Tonifiez votre corps avec cette nouvelle prestation ultra-efficace ! &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                ☀️ L'heure du Summer Body 2025 à sonné ! ☀️ C'est maintenant que tout commence ! Restez connectés 📅
             </marquee>
         </div>
     );
